bf16574830bdc453d2263491a5dfc33927075e41
Tsvetelina Gadzhelova <tsvetelina.gadzhelova@gmail.com>
2025-10-14 12:20:07 +0300

    chore: initial Spring Boot project setup

---
+++ src/main/java/app/popdapplication/PopdApplication.java
@@ -0,0 +1,13 @@
+package app.popdapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopdApplication.class, args);
+    }
+
+}
